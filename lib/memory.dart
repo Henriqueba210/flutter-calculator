d329b4537a3fd5b1310ed32ae18aed635fec6996
Henrique Barros de Almeida <henrique.almeida4@fatec.sp.gov.br>
2020-03-10 11:16:29 -0300

    primeira aula 10/3/2020

--- lib/memory.dart
+++ lib/memory.dart
@@ -1,14 +1,49 @@
 class Memory {
   String result = '0';
+  List<double> _buffer = [0.0, 0.0];
+  int _bufferIndex = 0;
+  bool _operationUsed = false;
+  String _operation;
+
+  static const OPERATIONS = "+-x%=";
 
   void applyCommand(String label) {
-    if (label == 'AC')
+    if (label == 'AC') {
       _clear();
-    else
-      result += label;
+    } else if (label == 'DEL') {
+      _deleteEndDigit();
+    } else if (OPERATIONS.contains(label)) {
+      _setOperation(label);
+    } else {
+      _addDigit(label);
+    }
   }
 
   _clear() {
     result = '0';
+    _operationUsed = false;
+    _buffer.setAll(0, [0.0,0.0]);
+    _bufferIndex = 0;
+    _operation = null;
+  }
+
+  void _addDigit(String digit) {
+    if (result == '0' && digit != '.') {
+      result = '';
+    }
+
+    if(digit == '.' && result.contains('.')){
+      digit = '';
+    }
+
+    result += digit;
+  }
+
+  void _deleteEndDigit() {
+    result = result.length == 1 ? '0' : result.substring(0, result.length - 1);
+  }
+
+  void _setOperation(String operation) {
+
   }
 }
