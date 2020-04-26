d9cec8d925950194aae1110602d6cf4a8b8da3de
Henrique Barros de Almeida <henrique.almeida4@fatec.sp.gov.br>
2020-04-26 18:51:35 -0300

    Adicionado tela de splash à aplicação

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 import 'package:flutter/services.dart';
 
 import 'calculator.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ));
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
     ]);
+    return new SplashScreen(
+      seconds: 4,
+      navigateAfterSeconds: new AfterSplash(),
+      title: new Text(
+        'Calculadora',
+        style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+      ),
+      image: new Image.asset('assets/icons/icon.png'),
+      backgroundColor: Colors.black,
+      styleTextUnderTheLoader: new TextStyle(),
+      photoSize: 50.0,
+      onClick: () => print("Flutter Egypt"),
+      loaderColor: Colors.deepOrange,
+    );
+  }
+}
 
-    return MaterialApp(
+class AfterSplash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Calculator(),
     );
   }
 }
-
-
